768890f3082d5c9285e3d7e744fa0ea939b4fdac
Steele <shrevesa@mail.uc.edu>
2023-09-19 04:24:06 -0400

    Create Role entity for persistence

---
+++ src/main/java/com/workoutbuilder/enterprise/entity/Role.java
@@ -0,0 +1,40 @@
+package com.workoutbuilder.enterprise.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Represents a role within the system. This entity maps to the "roles" table in the database.
+ * Roles are used to manage access control and permissions for users.
+ * A role has attributes like id and name, and can be associated with multiple users.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public @Data class Role {
+
+    /**
+     * Represents the unique ID of the role. This is auto-generated.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    /**
+     * Represents the name of the role. Each role has a unique name.
+     */
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    /**
+     * Represents the users associated with this role.
+     * A role can be associated with multiple users, and a user can have multiple roles.
+     */
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
