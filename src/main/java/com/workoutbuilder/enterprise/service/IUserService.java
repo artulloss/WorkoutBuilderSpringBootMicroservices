0795664bc6c54c89cda76134af30442eaec7a003
Steele <shrevesa@mail.uc.edu>
2023-09-19 04:25:59 -0400

    Create User Service Interface

---
+++ src/main/java/com/workoutbuilder/enterprise/service/IUserService.java
@@ -0,0 +1,25 @@
+package com.workoutbuilder.enterprise.service;
+
+import com.workoutbuilder.enterprise.dto.UserDTO;
+import com.workoutbuilder.enterprise.entity.User;
+
+/**
+ * Interface defining core operations related to user management.
+ */
+public interface IUserService {
+
+    /**
+     * Saves the provided UserDTO into the MySQL db.
+     *
+     * @param userDTO The user data transfer object containing the details of the user to be saved.
+     */
+    void saveUser(UserDTO userDTO);
+
+    /**
+     * Retrieves a user entity from the MySQL db based on their email.
+     *
+     * @param email The email address of the user to be retrieved.
+     * @return The User entity associated with the given email, or null if no user is found.
+     */
+    User findByEmail(String email);
+}
