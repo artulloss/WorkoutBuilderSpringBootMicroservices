e5dc3bc4f6f705d5113c7bdbe67669e90f1e0dd3
Adam Tulloss <artulloss@gmail.com>
2023-09-24 19:24:17 -0400

    After finishing lecture, used hashmap instead of arraylist

--- src/main/java/com/workoutbuilder/enterprise/dao/ExerciseDAOStub.java
+++ src/main/java/com/workoutbuilder/enterprise/dao/ExerciseDAOStub.java
@@ -4,34 +4,36 @@ import com.workoutbuilder.enterprise.dto.Exercise;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
 public class ExerciseDAOStub implements IExerciseDAO {
 
-    List<Exercise> exerciseList = new ArrayList<>();
+    HashMap<Number, Exercise> exercises = new HashMap<>();
 
     @Override
     public void saveExercise(Exercise exercise) throws Exception {
-        exerciseList.add(exercise);
+        exercises.put(exercise.getId(), exercise);
     }
 
     @Override
     public void updateExercise(Exercise exercise) throws Exception {
-        // We rebuild the list with the new exercise replace where the id matches
-        exerciseList = exerciseList.stream()
-                .map(existingExercise -> existingExercise.getId() == exercise.getId() ? exercise : existingExercise)
-                .collect(Collectors.toList());
+        if(exercises.containsKey(exercise.getId())) {
+            exercises.put(exercise.getId(), exercise); // Overwrites existing exercise
+        } else {
+            throw new Exception("Exercise does not exist");
+        }
     }
 
     @Override
     public void deleteExercise(Exercise exercise) throws Exception {
-        exerciseList.removeIf(existingExercise -> existingExercise.getId() == exercise.getId());
+        exercises.remove(exercise.getId());
     }
 
     @Override
     public List<Exercise> fetchAll() {
-        return exerciseList;
+        return new ArrayList<>(exercises.values());
     }
 }
