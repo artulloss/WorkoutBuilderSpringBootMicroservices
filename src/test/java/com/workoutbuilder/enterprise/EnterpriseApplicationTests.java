0c00f4114064b99356d28e0778a6fedf0c6b4a1b
Charlie Hayes <charlie@balancepoint.com>
2023-09-25 19:14:42 -0400

    Add IWorkoutService + stub and basic TDD test

--- src/test/java/com/workoutbuilder/enterprise/EnterpriseApplicationTests.java
+++ src/test/java/com/workoutbuilder/enterprise/EnterpriseApplicationTests.java
@@ -1,13 +1,32 @@
 package com.workoutbuilder.enterprise;
 
+import com.workoutbuilder.enterprise.dto.Workout;
+import com.workoutbuilder.enterprise.service.IWorkoutService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class EnterpriseApplicationTests {
 
+	@Autowired
+	private IWorkoutService workoutService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	/**
+	 * Tests fetchById method checks for workout type
+	 */
+	@Test
+	void fetchWorkoutTypeById() {
+		Workout workout = workoutService.fetchById(12);
+		String workoutType = workout.getWorkoutType();
+		assertEquals("Cardio", workoutType);
 	}
 
 }
