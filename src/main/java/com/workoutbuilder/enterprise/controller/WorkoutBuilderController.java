61a49a01ea604cb807804d09904faf61ec6e9bd0
Steele <shrevesa@mail.uc.edu>
2023-10-05 08:48:13 -0400

    Add JavaDoc to controller class

--- src/main/java/com/workoutbuilder/enterprise/controller/WorkoutBuilderController.java
+++ src/main/java/com/workoutbuilder/enterprise/controller/WorkoutBuilderController.java
@@ -11,39 +11,57 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller class responsible for handling requests related to workouts and exercises.
+ */
 @Controller
 public class WorkoutBuilderController {
 
     @Autowired
     IExerciseService exerciseService;
 
-
     /**
      * Maps the root URL ("/") to the index page.
      *
+     * @param model Model object for UI rendering.
      * @return name of the index view.
      */
     @GetMapping("/")
     public String index(Model model) {
         Exercise exercise = new Exercise();
         model.addAttribute("exercise", exercise);
-
         return "/index";
     }
 
-
+    /**
+     * Retrieves a list of all exercises.
+     *
+     * @return List of exercises.
+     */
     @GetMapping("api/exercise")
     @ResponseBody
     public List<Exercise> fetchAllExercises(){
         return exerciseService.findAll();
     }
 
+    /**
+     * Fetches an exercise by its ID.
+     *
+     * @param id ID of the exercise.
+     * @return Exercise with the given ID.
+     */
     @GetMapping("api/exercise/{id}")
     @ResponseBody
     public Exercise fetchExerciseById(@PathVariable("id") int id){
         return exerciseService.findById(id);
     }
 
+    /**
+     * Creates a new exercise.
+     *
+     * @param exercise Exercise object to be created.
+     * @return Newly created exercise.
+     */
     @PostMapping(value = "api/exercise", consumes = "application/json", produces = "application/json")
     @ResponseBody
     public Exercise createExercise(@RequestBody Exercise exercise) {
@@ -53,10 +71,15 @@ public class WorkoutBuilderController {
         } catch (Exception e) {
             //TODO LOG ERROR
         }
-
         return newExercise;
     }
 
+    /**
+     * Saves an exercise.
+     *
+     * @param exercise Exercise object to be saved.
+     * @return Redirection to the index page.
+     */
     @RequestMapping("api/saveExercise")
     public String saveExercise(Exercise exercise) {
         try {
@@ -68,6 +91,12 @@ public class WorkoutBuilderController {
         return "/index";
     }
 
+    /**
+     * Deletes an exercise by its ID.
+     *
+     * @param id ID of the exercise to be deleted.
+     * @return ResponseEntity with either HTTP OK status or HTTP Internal Server Error status.
+     */
     @DeleteMapping("api/exercise/{id}")
     public ResponseEntity deleteExercise(@PathVariable("id") int id) {
         try{
@@ -77,5 +106,4 @@ public class WorkoutBuilderController {
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
 }
