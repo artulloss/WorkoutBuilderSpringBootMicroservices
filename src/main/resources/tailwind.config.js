fe2f5e4717d2dedd33e09469dcbb1b713543caf8
Adam Tulloss <artulloss@gmail.com>
2023-10-05 22:38:24 -0400

    Tailwind - Setup a bundle that only includes the css classes we actually use. Build with tailwind npm script

---
+++ src/main/resources/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html", "./templates/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "deep-red": "#8B0000",
+        "lighter-red": "#A10A0A",
+      },
+    },
+  },
+  plugins: [],
+};
