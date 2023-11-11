397084758c19ff918ffbfb15514c63797d93aa65
Adam Tulloss <artulloss@gmail.com>
2023-11-10 23:33:09 -0500

    Require fields programatically depending on result of exercise select

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,22 +7,28 @@ jQuery(($) => {
         const form = qs('form#logWorkout');
         const addExerciseBtn = form.querySelector('#addExercise');
         const addFormFields = (e) => {
-          e && e.preventDefault();
-          exerciseCount++; // Increment the counter
-          const template = form.querySelector('#exerciseFields');
-          const clone = template.content.cloneNode(true);
-          const exercises = form.querySelector('.exercises');
+            e && e.preventDefault();
+            exerciseCount++; // Increment the counter
+            const template = form.querySelector('#exerciseFields');
+            const clone = template.content.cloneNode(true);
+            const exercises = form.querySelector('.exercises');
 
-          // Set a unique ID for the new select element
-          const select = clone.querySelector('select');
-          select.id = 'exerciseSelect' + exerciseCount;
+            // Set a unique ID for the new select element
+            const select = clone.querySelector('select');
+            select.id = 'exerciseSelect' + exerciseCount;
 
-          exercises.appendChild(clone);
-          addSelect2ToExercisePicker('#' + select.id); // Initialize select2 for the new select element
+            exercises.appendChild(clone);
+            addSelect2ToExercisePicker('#' + select.id); // Initialize select2 for the new select element
         };
         addExerciseBtn && addExerciseBtn.addEventListener('click', addFormFields);
         addFormFields();
     }
+
+    // Delegate event handling for the select change
+    $(document).on('change', 'form#logWorkout select', function() {
+        console.log('hello?', this);
+        updateRequiredFields(this);
+    });
 
     const addSelect2ToExercisePicker = (selector) => {
         $(selector).select2({
@@ -37,10 +43,8 @@ jQuery(($) => {
               },
               processResults: function (data, params) {
                 let results = [];
-                let id = 0;
                 data.forEach((d) => {
-                  results.push({ id: id, text: d.name })
-                  id++;
+                  results.push({ id: JSON.stringify(d), text: d.name })
                 })
                 return {
                   results: results,
@@ -50,6 +54,20 @@ jQuery(($) => {
             },
             minimumInputLength: 3,
         });
+    }
+
+    // Function to update required fields based on exercise type
+    const updateRequiredFields = (selectElement) => {
+        const selectedExercise = JSON.parse(selectElement.value);
+        console.log({selectedExercise});
+
+        const isCardio = selectedExercise.type === 'cardio'; // Check if selected exercise is cardio
+        const exerciseContainer = $(selectElement).closest('.exercise');
+
+        exerciseContainer.find('#exerciseSets').prop('required', !isCardio);
+        exerciseContainer.find('#exerciseReps').prop('required', !isCardio);
+        exerciseContainer.find('#exerciseWeight').prop('required', !isCardio);
+        exerciseContainer.find('#exerciseDuration').prop('required', isCardio);
     }
 
     addExercise();
