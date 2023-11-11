61e0487b989e6e28c8d7fa693ebe3ec3258c8d0f
Adam Tulloss <artulloss@gmail.com>
2023-11-10 22:53:53 -0500

    Multiple select2 issue fixed

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,44 +1,31 @@
 jQuery(($) => {
-
     const qs = document.querySelector.bind(document);
 
-//    const createExercise = () => {
-//        const form = qs('form#createExercise');
-//        form.addEventListener('submit', async (e) => {
-//            debugger;
-//            e.preventDefault();
-//            const formData = new FormData(form);
-//            const response = await fetch('/api/exercise', {
-//                method: 'POST',
-//                headers: {
-//                    'Content-Type': 'application/json',
-//                },
-//                body: JSON.stringify(Object.fromEntries(formData)),
-//            });
-//            if (response.ok) {
-//                window.location.reload(); // This could be done with DOM manipulation as well - for now this is easier
-//            }
-//        });
-//    }
+    let exerciseCount = 0; // Counter to keep track of the number of exercises added
 
     const addExercise = () => {
         const form = qs('form#logWorkout');
         const addExerciseBtn = form.querySelector('#addExercise');
         const addFormFields = (e) => {
           e && e.preventDefault();
+          exerciseCount++; // Increment the counter
           const template = form.querySelector('#exerciseFields');
           const clone = template.content.cloneNode(true);
           const exercises = form.querySelector('.exercises');
+
+          // Set a unique ID for the new select element
           const select = clone.querySelector('select');
+          select.id = 'exerciseSelect' + exerciseCount;
+
           exercises.appendChild(clone);
-          addSelect2ToExercisePickers(select);
+          addSelect2ToExercisePicker('#' + select.id); // Initialize select2 for the new select element
         };
         addExerciseBtn && addExerciseBtn.addEventListener('click', addFormFields);
         addFormFields();
     }
 
-    const addSelect2ToExercisePickers = () => {
-        $("form#logWorkout select").select2({
+    const addSelect2ToExercisePicker = (selector) => {
+        $(selector).select2({
             ajax: {
               url: "api/searchExerciseAutocomplete",
               dataType: "json",
@@ -49,15 +36,12 @@ jQuery(($) => {
                 };
               },
               processResults: function (data, params) {
-
                 let results = [];
                 let id = 0;
-
                 data.forEach((d) => {
                   results.push({ id: id, text: d })
                   id++;
                 })
-
                 return {
                   results: results,
                 };
